ed474d4078bf6ce896bf78d9179a0b17f51ccf3d
Zhivko Stoimchev <zstoimchev@outlook.com>
2026-03-19 22:04:32 +0100

    ZMX-#46 extend html request

--- src/main/java/dev/utils/Utils.java
+++ src/main/java/dev/utils/Utils.java
@@ -36,6 +36,10 @@ public class Utils {
     public static String buildHttpGet(String host) {
         return "GET / HTTP/1.1\r\n" +
                 "Host: " + host + "\r\n" +
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n" +
+                "Accept-Language: en-US,en;q=0.9\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
                 "Connection: close\r\n" +
                 "\r\n";
     }
