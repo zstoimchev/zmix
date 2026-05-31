723ffb745086ac9fc2791f631ad7e835240ba34c
Zhivko Stoimchev <zstoimchev@outlook.com>
2026-05-31 13:36:15 +0200

    ZMX-#46 add browser agent in raw HTTP request. Format HTTP response better

--- src/main/java/dev/network/CircuitManager.java
+++ src/main/java/dev/network/CircuitManager.java
@@ -335,8 +335,12 @@ public class CircuitManager {
 
         try {
             String fileName = "responses/" + System.nanoTime() + ".html";
+            logger.info("""
+                    HTML response:\s
+                    = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\s
+                    {}\s
+                    = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =""", response);
             String html = Utils.saveHttpResponseNaive(response, fileName);
-            setLastHtmlResponse(html);
             logger.info("Saved HTTP response to file: {}", fileName);
         } catch (FileNotFoundException e) {
             logger.error("Failed to save HTTP response to file", e);
--- src/main/java/dev/utils/Utils.java
+++ src/main/java/dev/utils/Utils.java
@@ -22,7 +22,10 @@ public class Utils {
         String lowercaseResponse = response.toLowerCase();
         int start = lowercaseResponse.indexOf("<!doctype html");
         if (start == -1) start = lowercaseResponse.indexOf("<html");
-        return (start != -1) ? response.substring(start) : response;
+        if (start == -1) return response;
+        int end = lowercaseResponse.lastIndexOf("</html>");
+        if (end == -1) return response.substring(start);
+        return response.substring(start, end + 7);
     }
 
     public static String saveHttpResponseNaive(String response, String fileName) throws FileNotFoundException {
@@ -36,6 +39,9 @@ public class Utils {
     public static String buildHttpGet(String host) {
         return "GET / HTTP/1.1\r\n" +
                 "Host: " + host + "\r\n" +
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" +
+                "Accept-Language: en-US,en;q=0.5\r\n" +
                 "Connection: close\r\n" +
                 "\r\n";
     }
