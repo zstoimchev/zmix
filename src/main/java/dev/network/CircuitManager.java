36fe4f750fb308b50c51558a9ccdc46bcae43a5c
Zhivko Stoimchev <zstoimchev@outlook.com>
2026-06-11 21:56:42 +0200

    ZMX-#78 implement print circuit routes

--- src/main/java/dev/network/CircuitManager.java
+++ src/main/java/dev/network/CircuitManager.java
@@ -432,15 +432,27 @@ public class CircuitManager {
         this.pendingKeys.clear();
     }
 
-    public String printCircuitPath() {
+    public String getMyCircuitRoute() {
         if (path == null || path.isEmpty()) return "No active circuit.";
 
-        String circuit = path.stream()
+        return path.stream()
                 .map(peer -> peer.getHost() + ":" + peer.getPort())
                 .reduce((a, b) -> a + " -> " + b)
                 .orElse("empty");
+    }
 
-        return "Circuit " + myCircuitId + ": " + circuit;
+    public String getRelayCircuitRoutes() {
+        if (relayCircuits.isEmpty()) return "No relay circuits yet.";
+
+        StringBuilder sb = new StringBuilder();
+
+        relayCircuits.forEach((id, relay) -> {
+            String prev = relay.previousHop != null ? relay.previousHop.getIp() + ":" + relay.previousHop.getPort() : "NULL";
+            String next = relay.nextHop != null ? relay.nextHop.getIp() + ":" + relay.nextHop.getPort() : "EXIT";
+            sb.append(prev).append(" -> ME -> ").append(next).append(", \t");
+        });
+
+        return sb.toString();
     }
 
     @AllArgsConstructor
